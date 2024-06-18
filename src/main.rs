52e0d141a1be263d3f92f5564819721be83c8124
Eric van der Toorn <eric@hologenomix.nl>
2024-06-18 12:25:40 +0200

    add the attribure parsing

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,113 @@
-use std::{
-    collections::{BTreeMap, VecDeque},
-    f32::consts::E,
-    io::Write,
-    path::PathBuf,
-};
+use std::{collections::BTreeMap, path::PathBuf};
 
 use clap::Parser;
-use quick_xml::{events::Event, name::QName};
+use quick_xml::events::Event;
 
 #[derive(clap::Parser)]
 struct Options {
-    #[arg(
-        short,
-        long,
-        default_value = "/projects/CLIENTS/q-2024-006/analysis/sampling/biosamples.out.xml"
-        // default_value = "/projects/CLIENTS/q-2024-006/analysis/sampling/sample.xml"
-    )]
+    #[arg(short, long)]
     pub input_file: PathBuf,
-    #[arg(short, long, default_value = "out.ndjson")]
+    #[arg(short, long)]
     pub output: PathBuf,
+    #[arg(short, long, default_value = ".")]
+    pub key_separator: String,
+    #[arg(short, long, default_value = "EXPERIMENT_PACKAGE")]
+    pub row_separator: String,
 }
 
 fn main() {
     run(Options::parse()).unwrap()
 }
 
-fn to_key(stack: &VecDeque<Vec<u8>>) -> String {
+fn to_key(stack: &[Vec<u8>], separator: &str) -> String {
     let stacks: Vec<String> = stack
         .iter()
         .map(|x| String::from_utf8_lossy(x).to_string())
         .collect();
-
-    stacks.join(".")
+    stacks.join(separator)
 }
 
 fn run(options: Options) -> anyhow::Result<()> {
-    println!("Hello, world!");
     let mut f = quick_xml::Reader::from_file(&options.input_file)?;
+    let output_separator = &options.row_separator.as_bytes();
     let mut buf = Vec::new();
-    let mut stack = VecDeque::new();
+    let mut stack = Vec::new();
     let mut output_vec = Vec::new();
     let mut dictionary = BTreeMap::new();
     loop {
         match f.read_event_into(&mut buf) {
-            Err(e) => panic!("Error at position {}: {:?}", f.buffer_position(), e),
+            // Parsing error -> Invalid XML
+            Err(e) => panic!(
+                "Invalid XML: Error at position {}: {:?}",
+                f.buffer_position(),
+                e
+            ),
+            // End of the file -> we're done, can start outputting
             Ok(Event::Eof) => break,
+            // We ignore these events as they should not have any content for us
             Ok(Event::DocType(_) | Event::CData(_) | Event::PI(_)) => {}
-            Ok(Event::Start(e)) => stack.push_back(e.to_owned().name().0.to_vec()),
+            // Push onto the stack and add the attributes as values
+            Ok(Event::Start(e)) => {
+                let e = e.to_owned();
+                for attr in e.attributes() {
+                    let attr = attr.expect("Failed to read attribute");
+                    let mut stack_key = stack.clone();
+                    stack_key.push(attr.key.0.to_vec());
+
+                    let k = to_key(&stack_key, &options.key_separator);
+                    let unescaped = attr.unescape_value();
+                    let value = if let Ok(unescaped) = unescaped {
+                        unescaped.to_string()
+                    } else {
+                        let raw = attr.value.to_vec();
+                        let raw = String::from_utf8(raw)
+                            .expect("Non-utf8 attribute value")
+                            .replace("&amp;", "&");
+                        raw
+                    };
+                    let entry = dictionary.entry(k).or_insert(Vec::new());
+                    entry.push(value);
+                }
+
+                stack.push(e.name().0.to_vec());
+            }
+            // Pop off the stack, if we reach the row separator we output the dictionary
             Ok(Event::End(e)) => {
-                let popped_e = stack.pop_back().expect("End event without start");
+                // We perform a check to see that we're nesting correctly.
+                let popped_e = stack.pop().expect("End event without start");
                 let start_word = popped_e;
                 let end_word = e.name().0;
                 assert_eq!(start_word, end_word, "Start and end words do not match");
-                if end_word == b"EXPERIMENT_PACKAGE" {
-                    // eprintln!("Finished a sample");
-                    // prepare an ndjson line
+                if end_word == *output_separator {
                     output_vec.push(dictionary.clone());
                     dictionary.clear();
                 }
-                // if stack.len() == 1{
-
-                //     eprintln!("Length: {}", stack.len());
-                //     eprintln!("Remaining: {:?}", String::from_utf8_lossy(&stack[0]).to_string());
-                // }
-                // eprintln!("Event: {:?} popped: {:?}", e.clone(), popped_e.clone());
-                // assert_eq!(e, popped_e);
             }
+            // These are some of the values we care about
             Ok(Event::Text(mut e)) => {
                 if e.inplace_trim_end() || e.inplace_trim_start() {
                     continue;
                 }
-                let k = to_key(&stack);
+                let k = to_key(&stack[..], &options.key_separator);
                 let entry = dictionary.entry(k).or_insert(Vec::new());
-                // if entry.len() > 0 {
-                //     eprintln!("Duplicate key: {}", k);
-                // }
-                entry.push(e.unescape().unwrap().to_string());
-                // dictionary
-                //     .entry(k)
-                //     .or_insert(Vec::new())
-                //     .push(e.unescape().unwrap().to_string());
+                let v = e.unescape().expect("non-utf8").to_string();
+                entry.push(v);
             }
-            Ok(_event) => {
-                // eprintln!("Event: {:?}", event);
-            }
+            Ok(_event) => {}
         }
         buf.clear();
     }
+
+    // Check that the stack and dictionary are empty
+    if !stack.is_empty() {
+        eprintln!("Stack is not empty");
+        eprintln!("{:?}", stack);
+    }
+    if !dictionary.is_empty() {
+        eprintln!("Dictionary is not empty");
+        eprintln!("{:?}", dictionary);
+    }
     eprintln!("Finished reading the file");
     serde_jsonlines::write_json_lines(&options.output, &output_vec)?;
-    // let writer = std::io::BufWriter::new(std::fs::File::create(&options.output)?);
-    // // let value =
-    // serde_json::to_writer(writer, &dictionary)?;
-    // writer.flush();
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    const INPUT_FILE: &str = "/projects/CLIENTS/q-2024-006/analysis/sampling/biosamples.out.xml";
 }
